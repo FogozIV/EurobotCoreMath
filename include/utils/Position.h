026fee900e5d3d7f5dd3f102b572c8037a156e50
FogozIV <fogoziv@gmail.com>
2025-05-11 22:47:28 +0200

    first commit

---
+++ include/utils/Position.h
@@ -0,0 +1,118 @@
+
+#ifndef POSITION_H
+#define POSITION_H
+#include <optional>
+
+#include "Angle.h"
+#ifdef ARDUINO
+#include "Arduino.h"
+#endif
+#ifdef ARDUINO
+class Position : public Printable {
+#else
+class Position{
+#endif
+private:
+    double x;
+    double y;
+    Angle a;
+
+    public:
+
+    constexpr Position(double x=0.0f, double y=0.0f, Angle a=AngleConstants::ZERO): x(x), y(y), a(a) {
+    }
+    
+    constexpr double getX() const{
+        return x;
+    };
+    
+    constexpr double getY() const{
+        return y;
+    }
+
+    constexpr Angle getAngle() const{
+        return a;
+    }
+
+    constexpr void add(double x, double y, Angle a){
+        this->x += x;
+        this->y += y;
+        this->a += a;
+        this->a.warpAngle();
+    }
+
+    constexpr double getDistance() const {
+        return sqrt(pow(this->x, 2) + pow(this->y, 2));
+    }
+
+    constexpr Angle getVectorAngle() const {
+        return Angle::fromRadians(atan2(this->y, this->x));
+    }
+
+
+    constexpr Position operator+(const Position& pos) const{
+        return {this->x + pos.x, this->y + pos.y, this->a+ pos.a};
+    }
+
+    constexpr Position operator-(const Position& pos) const{
+        return {this->x - pos.x, this->y - pos.y, this->a - pos.a};
+    }
+
+    constexpr Position operator+=(const Position &rhs) {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        this->a += rhs.a;
+        this->a.warpAngle();
+        return *this;
+    }
+
+    constexpr Position operator*(const double rhs) const {
+        return {this->x * rhs, this->y * rhs, this->a * rhs};
+    }
+
+    constexpr Position operator/(const double rhs) const{
+        return {this->x / rhs, this->y / rhs, this->a / rhs};
+    }
+#ifdef ARDUINO
+    size_t Position::printTo(Print &p) const {
+        size_t length = 0;
+        length += p.print("x: ");
+        length += p.print(x);
+        length += p.print(", y: ");
+        length += p.print(y);
+        length += p.print(", angle: ");
+        length += p.print(a.toDegrees());
+        return length;
+    }
+#endif
+    constexpr Position getSinCosAngle() const {
+        return {cos(this->a.toRadians()), sin(this->a.toRadians())};
+    }
+
+    constexpr Position getNormalVector() const {
+        return {-sin(this->a.toRadians()), cos(this->a.toRadians())};
+    }
+};
+
+constexpr std::optional<Position> intersectLines(const Position&p1, const Position&n1, const Position&p2, const Position&n2) {
+    double det = -n1.getX()*n2.getY() + n1.getY()*n2.getX();
+    if (fabs(det) < 1e-6) {
+        return std::nullopt;
+    }
+    double dx = p1.getX() - p2.getX();
+    double dy = p1.getY() - p2.getY();
+
+    double s = (dx * -n2.getY() + dy * n2.getX()) / det;
+    Position center = {p1.getX() + s * n1.getX(), p1.getY() + s * n1.getY()};
+    return center;
+}
+
+constexpr std::optional<Position> intersectPerpendicularLine(const Position &p1, const Position &p2) {
+    Position n1 = p1.getNormalVector();
+    Position n2 = p2.getNormalVector();
+    return intersectLines(p1, n1, p2, n2);
+}
+
+
+
+#endif
